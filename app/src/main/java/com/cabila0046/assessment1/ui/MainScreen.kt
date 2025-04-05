4176caad6af9f680c8195464af9f640c1619c35a
cabila <cabila244@gmail.com>
2025-04-05 12:52:13 +0700

    Menambahkan inputan bagian tiga menggunakan dropdown

--- app/src/main/java/com/cabila0046/assessment1/ui/MainScreen.kt
+++ app/src/main/java/com/cabila0046/assessment1/ui/MainScreen.kt
@@ -1,14 +1,17 @@
 package com.cabila0046.assessment1.ui
 
 import android.content.res.Configuration
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -16,16 +19,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cabila0046.assessment1.R
 import com.cabila0046.assessment1.ui.theme.Assessment1Theme
-import androidx.compose.runtime.*
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -48,10 +54,20 @@ fun MainScreen()  {
         ScreenContent(Modifier.padding(innerPadding))
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
-    var pinjaman by remember { mutableStateOf(" ") }
-    var bunga by remember { mutableStateOf(" ") }
+    var loan by remember { mutableStateOf(" ") }
+    var interest by remember { mutableStateOf(" ") }
+
+    val choose = stringArrayResource(id = R.array.pilih_item)
+    var selectedChoose by remember { mutableStateOf(choose[0]) }
+    var custom by remember { mutableStateOf("") }
+    var expanded by remember { mutableStateOf(false) }
+
+
+
     Column(
         modifier = modifier.fillMaxSize().padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
@@ -59,11 +75,10 @@ fun ScreenContent(modifier: Modifier = Modifier) {
         Text(
             text = stringResource(id = R.string.intro),
             style = MaterialTheme.typography.bodyLarge,
-//
         )
         OutlinedTextField(
-            value = pinjaman,
-            onValueChange = { pinjaman = it },
+            value = loan,
+            onValueChange = { loan = it },
             label = { Text(text = stringResource(R.string.total_hutang)) },
             trailingIcon = { Text(text = " Rp ") },
             singleLine = true,
@@ -72,23 +87,65 @@ fun ScreenContent(modifier: Modifier = Modifier) {
                 imeAction = ImeAction.Next
             ),
             modifier = Modifier.fillMaxWidth()
-
         )
         OutlinedTextField(
-            value = bunga,
-            onValueChange = { bunga = it },
+            value = interest,
+            onValueChange = { interest = it },
             label = { Text(text = stringResource(R.string.bunga)) },
             trailingIcon = { Text(text = " % ") },
             singleLine = true,
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Done
+                imeAction = ImeAction.Next
             ),
             modifier = Modifier.fillMaxWidth()
         )
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded }
+        ) {
+            OutlinedTextField(
+                value = selectedChoose,
+                onValueChange = {},
+                readOnly = true,
+                label = { Text(stringResource(R.string.pilih)) },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                modifier = Modifier
+                    .menuAnchor()
+                    .fillMaxWidth()
+                    .clickable{ expanded = true }
+            )
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                choose.forEach { option ->
+                    DropdownMenuItem(
+                        text = { Text(option) },
+                        onClick = {
+                            selectedChoose = option
+                            expanded = false
+                        }
+                    )
+                }
+            }
+        }
+        if (selectedChoose == "other") {
+            OutlinedTextField(
+                value = custom,
+                onValueChange = { custom = it },
+                label = { Text("Enter Duration") },
+                trailingIcon = { Text(" month ") },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+
     }
-
-
 }
 
 
